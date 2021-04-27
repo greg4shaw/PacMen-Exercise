d302a9d011556c4d4e24fdb41b34080c0345fda7
greg <gman.international@gmail.com>
2021-04-27 08:36:47 -0400

    2021_04_27_Commit_2

--- pacmen.js
+++ pacmen.js
@@ -1,40 +1,71 @@
 
 var pos = 0;
-let pageWidth = window.innerWidth;
-
 //Array containing images
 const pacArray = [
-  ['./images/pacman1.png', './images/pacman2.png'], //Left to right 2 images
-  ['./images/pacman3.png', './images/pacman4.png'], // Right to left 2 images
+    ["./images/pacman1.png", "./images/pacman2.png"], //Left to right 2 images
+    ["./images/pacman3.png", "./images/pacman4.png"] // Right to left 2 images
 ];
-
 var direction = 0;
 //Left to right OR right to left (0 OR 1)
-var focus = 0;
-// Adding 1 and getting the modulus. So we it's even it will be 0. Odd will be 1.
+const pacMen = []; 
 
-// This function is called on mouse click. Every time it is called, it updates the PacMan image, position and direction on the screen.
-function Run() {
-  let img = document.getElementById('PacMan');
-  let imgWidth = img.width;
-  focus = (focus + 1) % 2;
-  direction = checkPageBounds(direction, imgWidth, pos, pageWidth);
-  img.src = pacArray[direction][focus];
-  if (direction) {
-    pos -= 20;
-    img.style.left = pos + 'px';
-  } else {
-    pos += 20;
-    img.style.left = pos + 'px';
-  }
+function setToRandom(scale) {
+    return {
+        x: Math.random() * scale,
+        y: Math.random() * scale
+    }
+}
+// Factory to make a PacMan at a random position with random velocity
+function makePac() {
+    // returns an object with random values scaled {x: 33, y: 21}
+    let velocity = setToRandom(10); // {x:?, y:?}
+    let position = setToRandom(200);
+    // Add image to div id = game
+    let game = document.getElementById('game');
+    let newimg = document.createElement('img');
+    newimg.style.position = 'absolute';
+    newimg.src = "./images/pacman1.png";
+    newimg.width = 100;
+    //newimg.height = 100;
+    //
+    // set position here 
+    //
+    newimg.style.left = position.x;
+    newimg.style.top = position.y;
+
+    // add new Child image to game
+    game.appendChild(newimg);
+    // return details in an object
+    return {
+        position,
+        velocity,
+        newimg
+    }
 }
 
-setInterval(Run, 200);
+function update() {
+    //loop over pacmen array and move each one and move image in DOM
+    pacMen.forEach((item) => {
+        checkCollisions(item)
+        item.position.x += item.velocity.x;
+        item.position.y += item.velocity.y;
 
-function checkPageBounds(direction, imgWidth, pos, pageWidth) {
+        item.newimg.style.left = item.position.x;
+        item.newimg.style.top = item.position.y;
+    })
+    setTimeout(update, 20);
+}
 
-  if (pos < 0 || pos > (pageWidth - imgWidth)) {
-            direction = 1 - direction;
-        }       
-  return direction;
+function checkCollisions(item) {
+    //
+    // detect collision with all walls and make pacman bounce
+    //
+    if (item.position.x + item.velocity.x + item.newimg.width > window.innerWidth || 
+    item.position.x + item.velocity.x < 0) item.velocity.x = -item.velocity.x;
+    if (item.position.y + item.velocity.y + item.newimg.height > window.innerHeight || 
+    item.position.y + item.velocity.y < 0) item.velocity.y = -item.velocity.y;
+}
+
+function makeOne() {
+    pacMen.push(makePac()); // add a new PacMan
 }
